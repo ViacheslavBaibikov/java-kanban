3a9d7cedf994b6b5c96dbbf10c5443ab953aa5ff
ViacheslavBaibikov <baibikov_vv@mail.ru>
2025-04-19 17:29:30 +0700

    delete obviously comments

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -1,7 +1,7 @@
 package model;
 
 public class Subtask extends Task {
-    private final int epicId;  // ID эпика, к которому принадлежит подзадача
+    private final int epicId;
 
     public Subtask(String name, String description, TaskStatus status, TaskPriority priority, int epicId) {
         super(name, description, status, priority);
